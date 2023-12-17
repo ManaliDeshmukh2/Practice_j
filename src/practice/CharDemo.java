0d918ad4ee746c0ab645b761f2f5fbc1a2e7399f
Admin <Admin@DESKTOP-GVLMBEC>
2023-12-17 20:29:34 +0530

    commit msg...

---
+++ src/practice/CharDemo.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class CharDemo {
+public static void main(String[]args)
+{
+	char c1='\u092E';
+	char c2='\u0928';
+	char c3='\u093E';
+	char c4='\u0932';
+	char c5='\u0940';
+	System.out.println(c1+""+c2+""+c3+""+c4+""+c5);
+	
+}
+}
